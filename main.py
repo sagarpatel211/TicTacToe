ee57818411b238e86264e5ca34884dc643f0f33f
Sagar Patel <2sagarpatel2@gmail.com>
2022-08-26 22:38:34 -0400

    Complete Game!

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 class TicTacToe:
+    games_played = 0
     def __init__(self):
         self.board = ['-', '-', '-',
                       '-', '-', '-',
                       '-', '-', '-']
         self.current_player = "X"
+        self.winner = None
+        self.game_running = True
+        TicTacToe.games_played += 1
 
-    def reset(self):
-        self.board = ['-', '-', '-',
-                      '-', '-', '-',
-                      '-', '-', '-']
-        self.current_player = "X"
 
-print("hi")
+game = TicTacToe()
+print("Welcome to Tic Tac Toe:")
+while(game.game_running):
+    print(game.current_player + "'s turn!")
+    game.game_running = False
+    print(TicTacToe.games_played)
